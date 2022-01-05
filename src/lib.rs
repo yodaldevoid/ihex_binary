70ab3cee79359a05ff844e51d6e399ae7ac857ee
Gabriel Smith <ga29smith@gmail.com>
2022-01-05 11:58:58 -0500

    Split file reading from record parsing

--- src/lib.rs
+++ src/lib.rs
@@ -11,13 +11,11 @@ pub enum LoadError {
     FailedOpen(#[source] io::Error),
     #[error("IO error when reading file")]
     FailedRead(#[source] io::Error),
-    #[error("Error while parsing IHEX records")]
-    Parsing(#[from] ReaderError),
     #[error("Error while unpacking IHEX into array")]
     Unpacking(#[from] UnpackingError),
 }
 
-pub fn load_ihex(
+pub fn load_file(
     file_path: &str,
     binary_size: usize,
     base_offset: usize,
@@ -28,20 +26,19 @@ pub fn load_ihex(
         .map_err(LoadError::FailedRead)?;
 
     let file_str = String::from_utf8_lossy(&file_buf[..]);
-    let ihex_reader = Reader::new(&file_str);
-    let ihex_result: Result<Vec<_>, _> = ihex_reader.collect();
-    let ihex_records = ihex_result?;
-    Ok(ihex_to_bytes(&ihex_records, binary_size, base_offset)?)
+    unpack_records(Reader::new(&file_str), binary_size, base_offset).map_err(LoadError::from)
 }
 
 #[derive(Debug, PartialEq, Error)]
 pub enum UnpackingError {
+    #[error("Error while parsing IHEX records")]
+    Parsing(#[from] ReaderError),
     #[error("Address ({0}) greater than binary size ({1})")]
     AddressTooHigh(usize, usize),
 }
 
-fn ihex_to_bytes(
-    recs: &[Record],
+pub fn unpack_records(
+    records: impl Iterator<Item = Result<Record, ReaderError>>,
     binary_size: usize,
     base_offset: usize,
 ) -> Result<(Vec<u8>, usize), UnpackingError> {
@@ -49,32 +46,37 @@ fn ihex_to_bytes(
     let mut binary = vec![0xFF; binary_size];
     let mut used_bytes = 0;
 
-    for rec in recs {
-        debug!("base_address=0x{:04X} rec={:?}", base_address, rec);
+    for rec in records {
         match rec {
-            Record::Data { offset, value } => {
-                let end_addr = base_address + *offset as usize + value.len();
-                if end_addr > binary_size {
-                    return Err(UnpackingError::AddressTooHigh(end_addr, binary_size));
-                }
+            Ok(rec) => {
+                debug!("base_address=0x{:04X} rec={:?}", base_address, rec);
+                match rec {
+                    Record::Data { offset, value } => {
+                        let end_addr = base_address + offset as usize + value.len();
+                        if end_addr > binary_size {
+                            return Err(UnpackingError::AddressTooHigh(end_addr, binary_size));
+                        }
 
-                used_bytes += value.len();
-                for (n, b) in value.iter().enumerate() {
-                    binary[base_address + *offset as usize + n] = *b;
+                        used_bytes += value.len();
+                        for (n, b) in value.iter().enumerate() {
+                            binary[base_address + offset as usize + n] = *b;
+                        }
+                    }
+                    Record::ExtendedSegmentAddress(base) => {
+                        base_address = ((base as usize) << 4) - base_offset
+                    }
+                    Record::ExtendedLinearAddress(base) => {
+                        base_address = ((base as usize) << 16) - base_offset
+                    }
+                    Record::EndOfFile => break,
+                    // Defines the start location for our program. This doesn't concern us so we
+                    // ignore it.
+                    Record::StartLinearAddress(_) | Record::StartSegmentAddress { .. } => {}
                 }
             }
-            Record::ExtendedSegmentAddress(base) => {
-                base_address = ((*base as usize) << 4) - base_offset
-            }
-            Record::ExtendedLinearAddress(base) => {
-                base_address = ((*base as usize) << 16) - base_offset
-            }
-            Record::EndOfFile => break,
-            // Defines the start location for our program. This doesn't concern us so we ignore it.
-            Record::StartLinearAddress(_) | Record::StartSegmentAddress { .. } => {}
+            Err(err) => return Err(UnpackingError::Parsing(err)),
         }
     }
 
     Ok((binary, used_bytes))
 }
-
